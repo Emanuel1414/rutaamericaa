0da8e3ba56a38d71f6be05795a6fd8683e83becc
Emanuel1414 <101937865+Emanuel1414@users.noreply.github.com>
2022-05-02 00:07:17 -0300

    Add files via upload

---
+++ procesar.php
@@ -0,0 +1,12 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$correoelectronico = $_POST['correoelectronico'];
+$mensaje = $_POST['mensaje'];
+
+  echo $nombre."<br>";
+  echo $correoelectronico."<br>";
+  echo $mensaje;
+
+
+?>
